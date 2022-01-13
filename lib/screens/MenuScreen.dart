8e00bea667e42e40508118b989363d58ac82c448
Romain Gréaume <r.greaume@beapp.fr>
2022-01-13 18:58:08 +0100

    Add MenuScreen fetchData

--- lib/screens/MenuScreen.dart
+++ lib/screens/MenuScreen.dart
@@ -1,15 +1,41 @@
+import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:luigi_pizza/dto/Menu.dart';
+import 'package:luigi_pizza/router/Router.dart' as router;
+import 'package:luigi_pizza/store/MenuStore.dart';
 
 class MenuScreen extends StatefulWidget {
-  const MenuScreen({Key? key}) : super(key: key);
+  final dynamic restaurantId;
+
+  const MenuScreen({Key? key, required this.restaurantId}) : super(key: key);
 
   @override
   _MenuScreenState createState() => _MenuScreenState();
 }
 
 class _MenuScreenState extends State<MenuScreen> {
+  Menu? restaurantMenu;
+
+  @override
+  void initState() {
+    super.initState();
+    _onMenuCreated();
+  }
+
+  Future<Menu> _onMenuCreated() async {
+    final futureMenu = await fetchMenu(widget.restaurantId);
+    setState(() {
+      restaurantMenu = futureMenu;
+    });
+    return futureMenu;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Center(
+        child: Text(restaurantMenu?.name ?? ""),
+      ),
+    );
   }
 }
--- lib/store/MenuStore.dart
+++ lib/store/MenuStore.dart
@@ -5,9 +5,11 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:luigi_pizza/dto/Menu.dart';
 
-Future<Menu> fetchMenu() async {
+// TODO: add restaurantId param
+Future<Menu> fetchMenu(restaurantId) async {
+  final int id = restaurantId;
   final response = await http.get(Uri.parse(
-      "https://private-anon-06cb6fff8f-pizzaapp.apiary-mock.com/restaurants/2/menu?category=Pizza&orderBy=rank"));
+      "https://private-anon-06cb6fff8f-pizzaapp.apiary-mock.com/restaurants/$id/menu?category=Pizza&orderBy=rank"));
 
   if (response.statusCode == 200) {
     final jsonResponse = json.decode(response.body);
