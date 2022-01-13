4434727ac41c31da46e767afa56c7dcb750468db
Romain Gréaume <r.greaume@beapp.fr>
2022-01-13 18:56:35 +0100

    Add markers to map

--- lib/screens/MapScreen.dart
+++ lib/screens/MapScreen.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:luigi_pizza/dto/Restaurant.dart';
-import 'package:luigi_pizza/map/Map.dart';
 import 'package:luigi_pizza/store/RestaurantStore.dart';
+import 'package:luigi_pizza/router/Router.dart' as router;
 
 class MapScreen extends StatefulWidget {
   const MapScreen({Key? key}) : super(key: key);
@@ -12,23 +12,52 @@ class MapScreen extends StatefulWidget {
 }
 
 class _MapScreenState extends State<MapScreen> {
-  Future<List<Restaurant>> futureRestaurants = fetchRestaurant();
   late GoogleMapController mapController;
+  final Map<String, Marker> _markers = {};
 
-  final LatLng _center = const LatLng(45.521563, -122.677433);
+  final LatLng _center = const LatLng(59.3157090, 18.0335070);
+
+  Future<void> _onMapCreated(GoogleMapController controller) async {
+    final restaurantsStore = await fetchRestaurant();
+    mapController = controller;
+
+    setState(() {
+      _markers.clear();
+      for (final restaurant in restaurantsStore) {
+        final marker = Marker(
+            markerId: MarkerId(restaurant.name),
+            position: LatLng(restaurant.lat, restaurant.long),
+            infoWindow: InfoWindow(
+                title: restaurant.name,
+                snippet: "${restaurant.street}\n${restaurant.city}",
+                //TODO: send restaurantId on push()
+                onTap: () => Navigator.pushNamed(context, router.menuScreen,
+                    arguments: restaurant.id)));
+        _markers[restaurant.name] = marker;
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
+    return Scaffold(
+      body: GoogleMap(
+        onMapCreated: _onMapCreated,
+        initialCameraPosition: CameraPosition(
+          target: _center,
+          zoom: 11.0,
+        ),
+        markers: _markers.values.toSet(),
+      ),
+    );
     return MaterialApp(
         home: Scaffold(
-      appBar: AppBar(
-        title: const Text('Fetch Data Example'),
-      ),
       body: Center(
           child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
           FutureBuilder<List<Restaurant>>(
-            future: futureRestaurants,
+            // future: futureRestaurants,
             builder: (context, snapshot) {
               if (snapshot.hasData) {
                 List<Restaurant>? restaurants = snapshot.data;
@@ -55,7 +84,7 @@ class _MapScreenState extends State<MapScreen> {
             height: 300,
             width: 300,
             color: Colors.red,
-            child: const MapBox(),
+            // child: const MapBox(),
           )
         ],
       )),
