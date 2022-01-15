5a49fe0c3e7280ff244c79822c68232d2f62d9f4
Romain Gréaume <r.greaume@beapp.fr>
2022-01-15 21:08:04 +0100

    Separate carousel from MenuScreen & add toppings && images

---
+++ lib/components/CarouselMenu.dart
@@ -0,0 +1,154 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:luigi_pizza/dto/Menu.dart';
+import 'package:luigi_pizza/dto/Restaurant.dart';
+import 'package:luigi_pizza/store/MenuStore.dart';
+
+class CarouselMenu extends StatefulWidget {
+  final Restaurant restaurantInfos;
+  const CarouselMenu({Key? key, required this.restaurantInfos})
+      : super(key: key);
+
+  @override
+  _CarouselMenuState createState() => _CarouselMenuState();
+}
+
+class _CarouselMenuState extends State<CarouselMenu> {
+  final PageController controller = PageController();
+  Future<List<Menu>>? restaurantMenu;
+
+  @override
+  void initState() {
+    super.initState();
+    // we are not really calling setState() within initState() because _onMenuCreated is asynchronous so initState() is already called
+    // TODO: find a way to call setState() and set restaurantMenu on another place
+    setState(() {
+      restaurantMenu = _onMenuCreated();
+    });
+  }
+
+  Future<List<Menu>> _onMenuCreated() async {
+    List<Menu> futureMenu = await fetchMenu(widget.restaurantInfos.id);
+
+    return futureMenu;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.red,
+      height: 600,
+      child: PageView(
+        controller: controller,
+        children: [
+          FutureBuilder<List<Menu>>(
+              future: restaurantMenu,
+              builder: (context, snapshot) {
+                if (snapshot.hasData) {
+                  List<Menu>? menu = snapshot.data;
+
+                  return ListView.separated(
+                    scrollDirection: Axis.vertical,
+                    shrinkWrap: true,
+                    itemCount: menu!.length,
+                    itemBuilder: (BuildContext context, int index) {
+                      if (menu[index].category == "Pizza") {
+                        return GestureDetector(
+                            onTap: () {
+                              print(menu[index].name);
+                            },
+                            child: Container(
+                                height: 150,
+                                color: Colors.green,
+                                child: Row(
+                                  children: [
+                                    Container(
+                                      height: 80,
+                                      padding: const EdgeInsets.only(
+                                          left: 20, right: 20),
+                                      // width: 50,
+                                      child: Image.asset(
+                                          'assets/images/pizza.png'),
+                                    ),
+                                    Column(children: [
+                                      Text(menu[index].name),
+                                      if (menu.isNotEmpty)
+                                        for (var topping
+                                            in menu[index].topping!)
+                                          Text(topping)
+                                    ]),
+                                  ],
+                                )));
+                      } else {
+                        return Container();
+                      }
+                    },
+                    separatorBuilder: (BuildContext context, int index) =>
+                        const Divider(),
+                  );
+                } else if (snapshot.hasError) {
+                  return const Text("error");
+                }
+                return const CircularProgressIndicator();
+              }),
+          FutureBuilder<List<Menu>>(
+              future: restaurantMenu,
+              builder: (context, snapshot) {
+                if (snapshot.hasData) {
+                  List<Menu>? menu = snapshot.data;
+
+                  return ListView.builder(
+                      scrollDirection: Axis.vertical,
+                      shrinkWrap: true,
+                      itemCount: menu!.length,
+                      itemBuilder: (BuildContext context, int index) {
+                        if (menu[index].category == "Dryck") {
+                          return Container(
+                            height: 75,
+                            color: Colors.green,
+                            child: Center(
+                              child: Text(menu[index].category),
+                            ),
+                          );
+                        } else {
+                          return Container();
+                        }
+                      });
+                } else if (snapshot.hasError) {
+                  return const Text("error");
+                }
+                return const CircularProgressIndicator();
+              }),
+          FutureBuilder<List<Menu>>(
+              future: restaurantMenu,
+              builder: (context, snapshot) {
+                if (snapshot.hasData) {
+                  List<Menu>? menu = snapshot.data;
+
+                  return ListView.builder(
+                      scrollDirection: Axis.vertical,
+                      shrinkWrap: true,
+                      itemCount: menu!.length,
+                      itemBuilder: (BuildContext context, int index) {
+                        if (menu[index].category == "TillbehÃ¶r") {
+                          return Container(
+                            height: 75,
+                            color: Colors.green,
+                            child: Center(
+                              child: Text(menu[index].category),
+                            ),
+                          );
+                        } else {
+                          return Container();
+                        }
+                      });
+                } else if (snapshot.hasError) {
+                  return const Text("error");
+                }
+                return const CircularProgressIndicator();
+              }),
+        ],
+      ),
+    );
+  }
+}
--- lib/screens/MenuScreen.dart
+++ lib/screens/MenuScreen.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
-import 'package:luigi_pizza/dto/Menu.dart';
+import 'package:luigi_pizza/components/carouselMenu.dart';
 import 'package:luigi_pizza/dto/Restaurant.dart';
-import 'package:luigi_pizza/router/Router.dart' as router;
-import 'package:luigi_pizza/store/MenuStore.dart';
 
 class MenuScreen extends StatefulWidget {
   final Restaurant restaurantInfos;
@@ -15,28 +12,6 @@ class MenuScreen extends StatefulWidget {
 }
 
 class _MenuScreenState extends State<MenuScreen> {
-  Future<List<Menu>>? restaurantMenu;
-
-  var others;
-
-  @override
-  void initState() {
-    super.initState();
-    // we are not really calling setState() within initState() because _onMenuCreated is asynchronous so initState() is already called
-    // TODO: find a way to call setState() and set restaurantMenu on another place
-    setState(() {
-      restaurantMenu = _onMenuCreated();
-    });
-  }
-
-  Future<List<Menu>> _onMenuCreated() async {
-    List<Menu> futureMenu = await fetchMenu(widget.restaurantInfos.id);
-
-    return futureMenu;
-  }
-
-  final PageController controller = PageController();
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -55,103 +30,7 @@ class _MenuScreenState extends State<MenuScreen> {
               style: const TextStyle(fontFamily: "title", fontSize: 50),
             ),
           ),
-          Container(
-            color: Colors.red,
-            height: 600,
-            child: PageView(
-              controller: controller,
-              children: [
-                FutureBuilder<List<Menu>>(
-                    future: restaurantMenu,
-                    builder: (context, snapshot) {
-                      if (snapshot.hasData) {
-                        List<Menu>? menu = snapshot.data;
-
-                        return ListView.builder(
-                            scrollDirection: Axis.vertical,
-                            shrinkWrap: true,
-                            itemCount: menu!.length,
-                            itemBuilder: (BuildContext context, int index) {
-                              if (menu[index].category == "Pizza") {
-                                return GestureDetector(
-                                    onTap: () {
-                                      print(menu[index].name);
-                                    },
-                                    child: Container(
-                                      height: 75,
-                                      color: Colors.green,
-                                      child: Center(
-                                        child: Text(menu[index].category),
-                                      ),
-                                    ));
-                              } else {
-                                return Container();
-                              }
-                            });
-                      } else if (snapshot.hasError) {
-                        return const Text("error");
-                      }
-                      return const CircularProgressIndicator();
-                    }),
-                FutureBuilder<List<Menu>>(
-                    future: restaurantMenu,
-                    builder: (context, snapshot) {
-                      if (snapshot.hasData) {
-                        List<Menu>? menu = snapshot.data;
-
-                        return ListView.builder(
-                            scrollDirection: Axis.vertical,
-                            shrinkWrap: true,
-                            itemCount: menu!.length,
-                            itemBuilder: (BuildContext context, int index) {
-                              if (menu[index].category == "Dryck") {
-                                return Container(
-                                  height: 75,
-                                  color: Colors.green,
-                                  child: Center(
-                                    child: Text(menu[index].category),
-                                  ),
-                                );
-                              } else {
-                                return Container();
-                              }
-                            });
-                      } else if (snapshot.hasError) {
-                        return const Text("error");
-                      }
-                      return const CircularProgressIndicator();
-                    }),
-                FutureBuilder<List<Menu>>(
-                    future: restaurantMenu,
-                    builder: (context, snapshot) {
-                      if (snapshot.hasData) {
-                        List<Menu>? menu = snapshot.data;
-
-                        return ListView.builder(
-                            scrollDirection: Axis.vertical,
-                            shrinkWrap: true,
-                            itemCount: menu!.length,
-                            itemBuilder: (BuildContext context, int index) {
-                              if (menu[index].category == "TillbehÃ¶r") {
-                                return Container(
-                                  height: 75,
-                                  color: Colors.green,
-                                  child: Center(
-                                    child: Text(menu[index].category),
-                                  ),
-                                );
-                              } else {
-                                return Container();
-                              }
-                            });
-                      } else if (snapshot.hasError) {
-                        return const Text("error");
-                      }
-                      return const CircularProgressIndicator();
-                    }),
-              ],
-            ),
-          )
+          CarouselMenu(restaurantInfos: widget.restaurantInfos),
         ],
       ),
     );
