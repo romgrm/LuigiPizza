6a608d99258d2fec1d53f4d85fcc679646b588bf
Romain Gréaume <r.greaume@beapp.fr>
2022-01-21 17:36:40 +0100

    Manage state

--- lib/screens/MenuScreen.dart
+++ lib/screens/MenuScreen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:luigi_pizza/components/carouselMenu.dart';
 import 'package:luigi_pizza/dto/Restaurant.dart';
 import 'package:luigi_pizza/providers/ArticlesStore.dart';
@@ -17,21 +16,14 @@ class MenuScreen extends StatefulWidget {
 
 class _MenuScreenState extends State<MenuScreen> {
   bool display = false;
-  // List articlesStore = [];
-
-  // void _removeItemFromStore(int index) {
-  //   setState(() {
-  //     articlesStore.removeAt(index);
-  //   });
-  // }
 
   @override
   Widget build(BuildContext context) {
+    // Use Provider.of() for access to data (getter/setter) without rebuild UI (listen: false)
     ArticlesStore dataStore =
         Provider.of<ArticlesStore>(context, listen: false);
 
     return Scaffold(
-      // backgroundColor: Color(0xff856CD4),
       body: Column(
         mainAxisAlignment: MainAxisAlignment.start,
         mainAxisSize: MainAxisSize.max,
@@ -40,7 +32,6 @@ class _MenuScreenState extends State<MenuScreen> {
           Container(
             margin: EdgeInsets.only(top: 50),
             alignment: Alignment.topCenter,
-            // color: Colors.yellow,
             child: Text(
               "${widget.restaurantInfos.name}'s menu",
               style: TextStyle(fontFamily: "title", fontSize: 50),
@@ -52,14 +43,10 @@ class _MenuScreenState extends State<MenuScreen> {
               CarouselMenu(
                 restaurantInfos: widget.restaurantInfos,
                 articlesSelected: (List articlesSelected) {
-                  // setState(() {
-                  //   display = true;
-                  //   articlesStore = articlesSelected;
-                  // });
                   setState(() {
                     display = true;
                   });
-
+                  // Set store's ArticleStore with Carousel Child, without change UI's Carousel
                   dataStore.addItem(articlesSelected);
                 },
               ),
@@ -67,61 +54,59 @@ class _MenuScreenState extends State<MenuScreen> {
                   ? Align(
                       alignment: Alignment.bottomCenter,
                       child: Container(
-                        padding: EdgeInsets.only(bottom: 30, top: 20),
-                        child: CupertinoButton(
-                            color: Color(0xff352B54),
-                            disabledColor: Color(0xff352B54),
-                            onPressed: () {
-                              showModalBottomSheet(
-                                  context: context,
-                                  shape: const RoundedRectangleBorder(
-                                      borderRadius: BorderRadius.only(
-                                          topLeft: Radius.circular(20),
-                                          topRight: Radius.circular(20))),
-                                  builder: (BuildContext builder) =>
-                                      Consumer<ArticlesStore>(builder:
-                                          (context, articleStore, child) {
-                                        return ListView.separated(
-                                          itemCount: articleStore.store.length,
-                                          itemBuilder: (context, index) {
-                                            return ListTile(
-                                              leading: Image.asset(
-                                                'assets/images/pizza.png',
-                                              ),
-                                              title: Text(
-                                                  articleStore.store[index]),
-                                              trailing: GestureDetector(
-                                                child:
-                                                    Icon(Icons.delete_outline),
+                          padding: EdgeInsets.only(bottom: 30, top: 20),
+                          child: CupertinoButton(
+                              color: Color(0xff352B54),
+                              disabledColor: Color(0xff352B54),
+                              onPressed: () {
+                                showModalBottomSheet(
+                                    context: context,
+                                    shape: const RoundedRectangleBorder(
+                                        borderRadius: BorderRadius.only(
+                                            topLeft: Radius.circular(20),
+                                            topRight: Radius.circular(20))),
+                                    builder: (BuildContext builder) =>
+                                        // Listen ArticleStore's data and Rebuild UI of all elements who have Consumer
+                                        Consumer<ArticlesStore>(builder:
+                                            (context, articleStore, child) {
+                                          return ListView.separated(
+                                            itemCount:
+                                                articleStore.store.length,
+                                            itemBuilder: (context, index) {
+                                              return ListTile(
+                                                leading: Image.asset(
+                                                  'assets/images/pizza.png',
+                                                ),
+                                                title: Text(
+                                                    articleStore.store[index]),
+                                                trailing: GestureDetector(
+                                                  child: const Icon(
+                                                      Icons.delete_outline),
+                                                  onTap: () {
+                                                    articleStore
+                                                        .removeItem(index);
+                                                  },
+                                                ),
+                                                contentPadding:
+                                                    const EdgeInsets.all(20),
                                                 onTap: () {
-                                                  //  _removeItemFromStore(
-                                                  //     index);
-                                                  // Provider.of<ArticlesStore>(
-                                                  //         context,
-                                                  //         listen: false)
-                                                  //     .removeItem(index);
-                                                  dataStore.removeItem(index);
+                                                  print(index);
                                                 },
-                                              ),
-                                              contentPadding:
-                                                  EdgeInsets.all(20),
-                                              onTap: () {
-                                                print(index);
-                                              },
-                                            );
-                                          },
-                                          separatorBuilder: (context, index) {
-                                            return Divider();
-                                          },
-                                        );
-                                      }));
-                            },
-                            child: Consumer<ArticlesStore>(
-                                builder: (context, articleStore, child) {
-                              return Text(
-                                  "Afficher mon panier (${articleStore.store.length})");
-                            })),
-                      ),
+                                              );
+                                            },
+                                            separatorBuilder: (context, index) {
+                                              return const Divider();
+                                            },
+                                          );
+                                        }));
+                              },
+                              child:
+                                  // Same SubTree like Carousel but we want to change UI's Text so we use Consumer instead of Provider.of()
+                                  Consumer<ArticlesStore>(
+                                      builder: (context, articleStore, child) {
+                                return Text(
+                                    "Afficher mon panier (${articleStore.store.length})");
+                              }))),
                     )
                   : Container()
             ],
@@ -131,33 +116,3 @@ class _MenuScreenState extends State<MenuScreen> {
     );
   }
 }
-
-//  ListView.builder(
-//                                 shrinkWrap: true,
-//                                 itemCount: 1,
-//                                 itemBuilder: (context, index) {
-//                                   return ListView(
-//                                     shrinkWrap: true,
-//                                     children: ListTile
-//                                         .divideTiles(context: context, tiles: [
-//                                       for (var article in articlesStore)
-//                                         ListTile(
-//                                           leading: Image.asset(
-//                                             'assets/images/pizza.png',
-//                                           ),
-//                                           title: Text(article),
-//                                           trailing: GestureDetector(
-//                                             child: Icon(Icons.delete_outline),
-//                                             onTap: () {
-//                                               print(index);
-//                                             },
-//                                           ),
-//                                           contentPadding: EdgeInsets.all(20),
-//                                           onTap: () {
-//                                             print(articlesStore[index]);
-//                                           },
-//                                         )
-//                                     ]).toList(),
-//                                   );
-//                                 },
-//                               ),
